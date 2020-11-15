e4c4eedaeeb2d1be904e2492cc7ecaa6ee3a25ca
Adán Fernández <47212287+adanvetusta@users.noreply.github.com>
2020-11-15 23:14:50 +0100

    Añadidos componentes de header y footer

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -6,11 +6,14 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
-
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
